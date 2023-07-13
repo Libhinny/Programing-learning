d2655d87082a58d31967d8e38a17a66252410173
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-12 22:30:53 -0300

    Create importing_package.py

---
+++ importing_package.py
@@ -0,0 +1,16 @@
+# Import the math package
+import math as np
+from math import pi
+
+# Definition of radius
+r = 0.43
+
+# Calculate C
+C = 2*pi*r
+
+# Calculate A
+A = pi*(r*r)
+
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
