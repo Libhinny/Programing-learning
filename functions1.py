c540252a7c5102c9335dbbac4569c85d95424533
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-12 22:06:10 -0300

    Create functions1.py

---
+++ functions1.py
@@ -0,0 +1,12 @@
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place: place_up
+place_up = place.upper() # a saída será o nome poolhouse em caixa alta
+
+# Print out place and place_up
+print(place)
+print(place_up)
+
+# Print out the number of o's in place
+print(place.count("o"))
