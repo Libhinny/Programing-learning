8888eac322bde9fb9bc58def24f0e27c021e8d34
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-12 22:35:34 -0300

    Create selective_import.py

---
+++ selective_import.py
@@ -0,0 +1,12 @@
+# Import radians function of math package
+import math as np
+from math import radians
+from math import pi
+# Definition of radius
+r = 192500
+
+# Travel distance of Moon over 12 degrees. Store in dist.
+dist = np.radians (r * 12)
+
+# Print out dist
+print(dist)
