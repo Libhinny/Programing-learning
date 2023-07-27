aeb9c3a8f4aa07845749134b23939414cbfd5d52
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-27 15:11:46 -0300

    Create 2plot.py

---
+++ IntermediatePython/2plot.py
@@ -0,0 +1,13 @@
+# Print the last item of gdp_cap and life_exp
+print(gdp_cap)
+print(life_exp)
+
+
+# Make a line plot, gdp_cap on the x-axis, life_exp on the y-axis
+plt.plot(gdp_cap, life_exp)
+
+
+# Display the plot
+plt.show()
+
+# ao plotar sem importar a biblioteca, o gráfico se mostrou bem bagunçado e sem muito sentido, visualmente falando.
