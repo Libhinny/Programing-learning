039ad17dc1ff49758042f10a2d2761b5cde66fe5
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-27 15:20:54 -0300

    Create 4plot.py

---
+++ IntermediatePython/4plot.py
@@ -0,0 +1,9 @@
+# Import package
+import matplotlib.pyplot as plt 
+
+# Build Scatter plot
+# os valores já estavam disponíveis na plataforma, então não foi necessário declarar, apenas colocar na função.
+plt.scatter(pop, life_exp)
+
+# Show plot
+plt.show()
