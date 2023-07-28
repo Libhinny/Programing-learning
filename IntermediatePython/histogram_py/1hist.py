40951c39a6a7e71780e9eec30fcaef9efff7727a
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-28 14:00:15 -0300

    Create 1hist.py

---
+++ IntermediatePython/histogram_py/1hist.py
@@ -0,0 +1,5 @@
+# Create histogram of life_exp data
+plt.hist(life_exp) # não houve necessidade de especificar a quantidade de bins dentro do argumento, e o python por padrão deixa como sendo 10
+
+# Display histogram
+plt.show()
