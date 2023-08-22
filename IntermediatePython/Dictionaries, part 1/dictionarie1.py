3525b8058034a1a980cb35dd90dd432bf47f55a4
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-08-22 08:28:14 -0300

    Create dictionarie1.py

---
+++ IntermediatePython/Dictionaries, part 1/dictionarie1.py
@@ -0,0 +1,10 @@
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print out the keys in europe
+keys = europe.keys()
+print(keys)
+
+# Print out value that belongs to key 'norway'
+nor = europe['norway']
+print(nor)
